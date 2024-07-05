9073c7dd0ff606c1883e7d40557d5c933d652b39
IgorAp <igoraparecidorosa13@gmail.com>
2024-07-05 11:27:17 -0300

    add de 05/07/24

---
+++ javascript/for.js
@@ -0,0 +1,13 @@
+for(let i = -1; i<=10; i++){
+    i++ 
+    console.log(i)
+}
+
+/*for(let i = 0; i<=10; i++){
+    par = i%2
+    if(par == 0){
+    console.log(i)
+    }
+}
+*/
+
---
+++ javascript/tabuada.js
@@ -0,0 +1,10 @@
+let num = window.prompt('Digite um número e o sistema informara a multiplicação dele :D')
+
+for(let i = 0; i <=10; i++){
+    console.log("============")
+    let result = (num*i)
+    console.log(num+" x "+i+" = "+ result)
+}
+console.log("============")
+
+/*document.getElementById('resultadoTextarea').value = result;*/
