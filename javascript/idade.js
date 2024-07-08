2d41f056e837d574ad556a71bef652a97cfda869
IgorAp <igoraparecidorosa13@gmail.com>
2024-07-08 11:41:13 -0300

    add de 08/07/24

---
+++ javascript/idade.js
@@ -0,0 +1,10 @@
+for(let i = 1; i <=10; i++){
+    let idade = window.prompt('Qual a sua idade?')
+    if(idade >=18){
+        alert('Maior de Idade')
+    }
+
+    else{
+        alert('Menor de Idade')
+    }
+}
