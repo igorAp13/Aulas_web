6cb71572d4b6eca09230a7d1244f3f2f8713aaea
IgorAp <igoraparecidorosa13@gmail.com>
2024-07-23 12:05:08 -0300

    add de 23/07/24

---
+++ Perfumaria/PHP/salvarSMS.php
@@ -0,0 +1,24 @@
+<?php 
+    include 'conexaoBD.php';
+ 
+    $nome = filter_input(INPUT_POST, 'nome' , FILTER_SANITIZE_STRING);
+    $assunto = filter_input(INPUT_POST, 'assunto' , FILTER_SANITIZE_STRING);
+    $mensagem = filter_input(INPUT_POST, 'mensagem' , FILTER_SANITIZE_STRING);
+    $email = filter_input(INPUT_POST, 'email' , FILTER_SANITIZE_STRING);
+
+    $salvar = "INSERT INTO `mensagem`(`nome`,`assunto`,`mensagem`,`email`) VALUES ('$nome','$assunto','$mensagem','$email')";                                    
+
+    $resultado = mysqli_query($conexao,$salvar);
+
+    if($resultado){
+        echo"<script>
+            window.location.href ='../Contato.html?status=success';
+             </script>";
+    }
+    
+    else{
+        echo"<script>
+        window.location.href ='../Contato.html?status=error';
+        </script>";
+    }
+?>
