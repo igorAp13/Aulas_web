a93a814dea9a373cfb35f691a3e660aeb9f7797d
IgorAp <igoraparecidorosa13@gmail.com>
2024-07-30 12:00:06 -0300

    add de 30/07/24

--- bootstrap/PHP/cadastrar_aluno.php
+++ bootstrap/PHP/cadastrar_aluno.php
@@ -11,7 +11,7 @@
         if(empty($nome) || empty($cpf) || empty($celular) || empty($whatsapp) || empty($email) || empty($categoria)){
             echo"<script>
                     alert('Preencha todos os campos!!!');
-                    window.location.href ='../HTML/cadAluno.html;'
+                    window.location.href ='../HTML/cadAluno.php;'
                 </script>";
             exit;
         }
@@ -24,25 +24,58 @@
         if($declaracao->num_rows>0){
             echo"<script>
                   alert('Esse cpf já foi cadastrado no sistema!!!');
-                  window.location.href ='../HTML/cadAluno.html;'
+                  window.location.href ='../HTML/cadAluno.php'
                 </script>";
+             exit;
              $declaracao->close();
             }
         //$stmt
-         $declaracao = $conexao->prepare("INSERT INTO `cadaluno`(`nome`,`cpf`,`celular`,`whatsapp`,`email`,`categoria`) VALUES (?,?,?,?,?,?)");
+        $declaracao = $conexao->prepare("INSERT INTO `cadaluno`(`nome`,`cpf`,`celular`,`whatsapp`,`email`,`categoria`) VALUES (?,?,?,?,?,?)");
 
         $declaracao->bind_param("ssssss", $nome, $cpf, $celular, $whatsapp, $email, $categoria);
         if ($declaracao->execute()){
-            echo" <script>
-                alert('Aluno cadastrado com sucesso');
-                 window.location.href ='../HTML/cadAluno.html'
-            </script>";
-    
+            echo"<script>
+                    alert('Aluno cadastrado com sucesso');
+                    window.location.href ='../HTML/cadAluno.php'
+                </script>";
         }
 
         else{
             echo "Erro ao cadastro aluno: " . $declaracao->error;
         }
+    }
+
+    if(isset($_POST['btnCpf'])){
+        $buscar_cpf = $_POST['cpf'];
+
+        //Consulta para buscar CPF
+
+        $stmt = $conexao->prepare("SELECT nome, cpf, celular, whatsapp, email, categoria FROM cadaluno WHERE cpf = ?");
+        $stmt->bind_param("s", $buscar_cpf);
+        $stmt->execute();
+        $stmt->store_result();
+
+        if($stmt->num_rows > 0){
+            $stmt->bind_result($nome, $cpf, $celular, $whatsapp, $email, $categoria);
+            $stmt->fetch();
+            $_SESSION['nome'] = $nome;
+            $_SESSION['cpf'] = $cpf;
+            $_SESSION['celular'] = $celular;
+            $_SESSION['whatsapp'] = $whatsapp;
+            $_SESSION['email'] = $email;
+            $_SESSION['categoria'] = $categoria;
+            header("Location ../HTML/cad_aluno.php");
+        }
+
+        else{
+            echo"<script>
+                    alert ('CPF não encontrado!')
+                 </script>";
+        }
+
+        //Fecha a declaração e a conexão
+        $stmt->close();
+        $conexao->close();
     }
 
 ?>
