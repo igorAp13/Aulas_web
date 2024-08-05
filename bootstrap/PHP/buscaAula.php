ffaf38d359c3592665183a41d9adf58a46ba722a
IgorAp <igoraparecidorosa13@gmail.com>
2024-08-05 11:48:17 -0300

    add de 05/08/24

--- bootstrap/PHP/buscaAula.php
+++ bootstrap/PHP/buscaAula.php
@@ -1,7 +1,26 @@
 <?php
     include 'conexaoBD.php';
+    if(isset($_POST['delete_id'])){
+
+        $id = $_POST['delete_id'];
+        $declaracao = "DELETE FROM `aula` WHERE idaula = ? ;";
+        $declaracao = $conexao -> prepare($declaracao);
+        $declaracao -> bind_param("i", $id);
+ 
+
+        if($declaracao->execute()){
+            header("Location:../HTML/tabelaAula.php?message=Deletado com sucesso");
+        } 
+        else{
+            header("Location:../HTML/tabelaAula.php?message=Erro ao deletar");
+        }
+        $declaracao->close();
+        $conexao->close();
+        exit();
+    }
     $sql = "SELECT * FROM aula";
     $results = '';
+
     if(isset($_POST['instrutor']) || isset($_POST['nome_aluno'])){
         $instrutor = $_POST['instrutor'];
         $aluno = $_POST['nome_aluno'];   
@@ -16,17 +35,29 @@
     }
 
     $resultado = $conexao->query($sql);
+
     if($resultado->num_rows > 0){
         while ($linha = $resultado->fetch_assoc()){
             $formatarData = date('d/m/Y', strtotime($linha['data']));
             $formatarHora = date('H:i',strtotime($linha['hora']));
 
-            $results .= "<tr> 
+            $results .="<tr> 
                             <td>{$formatarData}</td>        
                             <td>{$formatarHora}</td>        
                             <td>{$linha['instrutor']}</td>        
                             <td>{$linha['aluno']}</td>        
-                            <td>{$linha['veiculo']}</td>        
+                            <td>{$linha['veiculo']}</td> 
+                            <td>
+                                <form action='../PHP/buscaAula.php' method='post' style='display:inline;'>
+                                    <input type='hidden' name='delete_id' value='{$linha['idaula']}'>
+                                    <button type='submit' name='delete'>Deletar</button>
+                                </form>
+
+                                <form action='#' method='get' style='display:inline;'>
+                                    <input type='hidden' name='id' value='{$linha['idaula']}'>
+                                    <button type='submit' name='edit'>Editar</button>
+                                </form>
+                            </td>
                         </tr>";
         }
     }   
