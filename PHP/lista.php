2874ca39370f5f2c4350552d732bfaab7b0bffcb
IgorAp <igoraparecidorosa13@gmail.com>
2024-08-14 11:39:39 -0300

    add de 14/08/24

---
+++ PHP/lista.php
@@ -0,0 +1,18 @@
+<?php
+    $frutas = array("maça","banana","laranja","uva");
+    //print_r($frutas);//print usado para imprimir a lista
+
+    //echo $frutas[3]."<br>";
+    //echo $frutas[0];
+
+    /*
+    foreach => nome lista(array)
+    em seguida cria-se uma nova vcariavel
+    foreach é um loop para lista
+    */
+
+    foreach($frutas as $lista){
+        echo "$lista"."<br>";
+    }
+
+?>
